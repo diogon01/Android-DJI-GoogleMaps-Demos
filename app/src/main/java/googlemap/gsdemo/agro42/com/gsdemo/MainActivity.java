4649cf358ffb60b389e5c8f7bb853b4612255084
Diogo Henrique <diogo.henrique.df@gmail.com>
2018-12-27 00:57:55 -0200

    Initial commit

---
+++ app/src/main/java/googlemap/gsdemo/agro42/com/gsdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package googlemap.gsdemo.agro42.com.gsdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
